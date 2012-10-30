d95b115881f909e805b164e77cbc124bfa704d37
ryanfiorini <ryanwfiorini@gmail.com>
2012-10-30 14:35:14 -0400

    init repo

---
+++ AppMobiWP8SimpleJqMobiTemplate/AppMobiPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using appMobiWebViewWP8;
+
+namespace AppMobiWP8SimpleJqMobiTemplate
+{
+    public partial class AppMobiPage : PhoneApplicationPage
+    {
+        public appMobiWebView amWebView;
+        
+        public AppMobiPage()
+        {
+            InitializeComponent();
+
+            amWebView = new appMobiWebView(webView, phoneApplicationPage, "html/index.html");
+        }
+    }
+}
